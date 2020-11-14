b20fa56fce74e8fef3f44389441cde46a2ed1922
GREX-Studio <56403846+GREX-Studio@users.noreply.github.com>
2020-11-14 03:24:56 -0300

    Update hipotenusa.py

--- hipotenusa.py
+++ hipotenusa.py
@@ -1,12 +1,9 @@
 import numpy as np
-#As funcoes np.() sao do numpy
-#O valor da hipotenusa e igual a raiz quadrada da soma do
-#quadrado dos lados A e B
 
 a=int(input("digite o Lado A ")) 
 b=int(input("digite o lado B "))
 c=int
-#variavel "d" representa o resultado
+#variavel "c" representa o resultado
 
 a=a**2
 print("lado a ", a)
